f339dd8ff03d9c9b68c0b62acbbd1db8edad9211
Lunamandla <lunamandlaluna@gmail.com>
2023-07-13 17:03:50 -0700

    My 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -13,10 +13,10 @@ int main(void)
 
 	//Evaluating size of variable
 	
-	printf("Size of char: %lu bytes($)\n",(unsigned long)sizeof(a));
-	printf("Size of int: %lu bytes($)\n",(unsigned long)sizeof (b));
-	printf("Size of long-int: %lu bytes($)\n",(unsigned long)sizeof(c));
-	printf("Size of long-long-int: %lu bytes($)\n",(unsigned long)sizeof(d));
-	printf("Size of float: %lu bytes($)\n",(unsigned long)sizeof(f));
+	printf("Size of char: %lu bytes(s)\n",(unsigned long)sizeof(a));
+	printf("Size of int: %lu bytes(s)\n",(unsigned long)sizeof (b));
+	printf("Size of long int: %lu bytes(s)\n",(unsigned long)sizeof(c));
+	printf("Size of long long int: %lu bytes(s)\n",(unsigned long)sizeof(d));
+	printf("Size of float: %lu bytes(s)\n",(unsigned long)sizeof(f));
 	return 0;
 }
