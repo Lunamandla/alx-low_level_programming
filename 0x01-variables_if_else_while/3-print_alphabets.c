645c1d8311ed33c455eb9e88a8b343ff3977d059
Lunamandla <lunamandlaluna@gmail.com>
2023-07-17 15:00:21 -0700

    My 3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -11,10 +11,10 @@ int main(void)
 	char a;
 
 	for (a = 'a'; a <= 'z'; a++)
-	printf("%c ", a);
+	printf("%c", a);
 
-	for (a = 'A'; a <= 'Z'; a++)
-	printf("%c ", a);
+	for(a = 'A'; a <= 'Z'; a++)
+	printf("%c", a);
 
 	return (0);
 }
