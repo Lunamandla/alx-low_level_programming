43913e841e8c28573e6c47184b4c12399ba0c0f0
Lunamandla <lunamandlaluna@gmail.com>
2023-07-17 16:10:11 -0700

    My 5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Programme that prints single numbers from zero to ten
+ *
+ * Return: Always (Success)
+ */
+int main(void)
+{
+	int a;
+
+	for (a = '0'; a <= '10'; a++)
+	
+	printf("%d", a);
+
+	return (0);
+}
+
