8e80dc4e1433f5dea6b2185134e3036b2630792c
Lunamandla <lunamandlaluna@gmail.com>
2023-07-17 13:45:42 -0700

    My 2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -4,15 +4,16 @@
 /**
  * main - programme that prints alphabets in lower cases
  *
- * Return: Alwasy (Success)
+ * Return: Always (Success)
  */
 int main(void)
 {
 	char ch = 'a';
-	printf("Lowercase alphabet :\n");
-	while(ch <= 'z')
+	printf("Lowercase alphabet :\n" );
+	while (ch <= 'z')
 	{
 	printf("%c ", ch);
+	ch++;
 	}
 	printf("\n");
 	return (0);
