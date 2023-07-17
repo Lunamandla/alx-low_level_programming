3ed8352430e8118efce7037831d4453325e852ad
Lunamandla <lunamandlaluna@gmail.com>
2023-07-17 15:45:59 -0700

    My 4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - programme that prints all alphabets accept e & q
+ *
+ * Return: Always (Success)
+ */
+int main(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		if (i != 'e' && i != 'q')
+		{
+		printf(c);
+		
+		}
+	}
+	printf('\n');
+
+	return (0);
+
+}
