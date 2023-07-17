725b1d7270bad49ea37bf7b4c0df34c266cd2a47
Lunamandla <lunamandlaluna@gmail.com>
2023-07-17 10:33:03 -0700

    My 0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -10,8 +10,6 @@ int main(void)
 	int n;
 	/*Check the condition*/
       
-      srand(time(0));
-      n = rand() - RAND_MAX / 2;
 	if (n > 0)
 	       /*if the statement is true the print the ff*/
 	       prinf("%d is positive\n",n);
