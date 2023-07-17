13d65241658e65fc97d64f2607ac338688838984
Lunamandla <lunamandlaluna@gmail.com>
2023-07-17 12:56:01 -0700

    My 1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -12,15 +12,15 @@ int main(void)
 
 	if (n > 5)
 	{
-		printf("%d and is greater than 5\n" )
+		printf("n and is greater than 5\n" )
 	}
 	else if (n == 0)
 	{
-		printf("%d and is 0\n" )
+		printf("n and is 0\n" )
 	}
 	else (n < 6 != 0)
 	{
-		printf("%d and is less than 6 and not 0\n" )
+		printf("n and is less than 6 and not 0\n" )
 	}
 	return (0);
 }
