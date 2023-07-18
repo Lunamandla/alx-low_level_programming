9db50ad7288814cc3442da28c239038136adedf9
Lunamandla <lunamandlaluna@gmail.com>
2023-07-17 17:30:38 -0700

    My 6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -11,7 +11,7 @@ int main(void)
 {
 	int a;
 
-	for (a = 0; a < 10; a++)
+	for (a = '0'; a <= '9`'; a++)
 	{
 		putchar(a);
 	}
@@ -19,6 +19,6 @@ int main(void)
 	putchar('\n');
 
 	return (0);
-		
+
 }
 
