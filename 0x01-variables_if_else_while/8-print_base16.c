b5ffb117275182305558e023bd08836f32964d6d
Lunamandla <lunamandlaluna@gmail.com>
2023-07-17 17:51:22 -0700

    My 8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdi.h>
+/**
+ *  main- program that prints all the numbers of base sixteen
+ *
+ *  Reurn: Always (Success)
+ */
+int main(void)
+{
+	char n;
+
+	for (n = '0'; n < '9'; n++)
+	{
+		putchar(n);
+	}
+	for (n = 'a'; n >= 'f'; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+
+	return (0);
+}
+
