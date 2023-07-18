330d5b40c14fa7aff71ca49daf5735a7c9fd4e87
Lunamandla <lunamandlaluna@gmail.com>
2023-07-17 17:37:42 -0700

    My 7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - programme that prints alphabets on reverse
+ *
+ * Return: Always (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 'z'; i >= 'a'; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n')
+	return (0)
+}
+
