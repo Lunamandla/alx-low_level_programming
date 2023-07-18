5f25e8a4cff8ca47b2135240d923c26702e29286
Lunamandla <lunamandlaluna@gmail.com>
2023-07-17 18:09:27 -0700

    My 9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - program that prints all single number in ascending order
+ *
+ * Return: Always (Success)
+ */
+int main(void)
+{
+	int a;
+
+	for (a = '0'; a <= '9'; a++)
+	{
+		putchar(a);
+
+	if (a != '9')
+	{
+		putchar(",");
+		putchar(' ');
+
+	}
+	}
+	putchar('\n');
+
+	return (0);
+}
