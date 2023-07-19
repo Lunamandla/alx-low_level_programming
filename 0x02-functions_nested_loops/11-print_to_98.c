5cee1118542d04a2170b6a2d976b96836584ed18
Lunamandla <lunamandlaluna@gmail.com>
2023-07-19 08:00:12 -0700

    My 11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -12,14 +12,14 @@ void print_to_98(int n)
 		{
 			if (n == 98)
 			{
-				printf("%d, n")
-					printf('\n')
+				printf("%d, n");
+					printf('\n');
 
 					break;
 			}
 			else
 			{
-				printf("%d, ", n)
+				printf("%d, ", n);
 			}
 		}
 	}
@@ -30,15 +30,14 @@ void print_to_98(int n)
 					if (n == 98)
 				{
 				
-				printf("%d, n")
-				printf('\n')
+				printf("%d, n");
+				printf('\n');
 				break;
 				}
 			else 
 			{
-				printf("%d, ", n)
+				printf("%d, ", n);
 			}
 		}
 	}
-
 }
