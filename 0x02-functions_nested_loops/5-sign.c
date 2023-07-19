dd2f209e15668d67167e62265da4f8785adf422b
Lunamandla <lunamandlaluna@gmail.com>
2023-07-18 19:40:01 -0700

    My 5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * main - code that prints the sign of a number 
+ *
+ * Return: 1 and print + if is greater than and 0 if is zero
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar(43);
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar(45);
+		return (-1);
+	}
+	else
+	{
+		_putchar(48);
+		return (0);
+	}
+}
