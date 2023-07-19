65d2c7d1b1a3f8671e3126ba77b1e7a01dc50ce0
Lunamandla <lunamandlaluna@gmail.com>
2023-07-18 19:14:19 -0700

    My 4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -4,11 +4,12 @@
  *
  * Return: 1 for alphabet character and 0 for anything else
  */
+
 int _isalpha(int c)
 {
 	if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
 	{
-			return (1);
+		return (1);
 	}
-			return (0);
+	return (0);
 }
