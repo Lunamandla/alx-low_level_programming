ebf2de733e874c8fdf1b33a4684881b81a73b7cf
Lunamandla <lunamandlaluna@gmail.com>
2023-07-19 07:55:54 -0700

    My 11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -8,7 +8,7 @@ void print_to_98(int n)
 {
 	if (n <= 98)
 	{
-		for (n <= 98; n++)
+		for (; n <= 98; n++)
 		{
 			if (n == 98)
 			{
@@ -23,9 +23,9 @@ void print_to_98(int n)
 			}
 		}
 	}
-			else		
-	{
-				for (n >= 98; n--)
+			else	
+			{
+				for (; n >= 98; n--)
 				{
 					if (n == 98)
 				{
