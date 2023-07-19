0570041ee6b080766dc40567b37c4393047412f1
Lunamandla <lunamandlaluna@gmail.com>
2023-07-18 18:11:19 -0700

    My 2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -9,13 +9,13 @@ void print_alphabet_x10(void)
 	char a;
 	int b = 0;
 
-	while (a <= 9)
+	while (b <= 9)
 	{
 		for (a = 'a'; a <= 'z'; a++)
 	{
 		_putchar(a);
 	}
-		_putchar('\n')
+		_putchar('\n');
 	
 		a++;
 	}
