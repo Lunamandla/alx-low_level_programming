a6f0dd879c48453e22e757222fdfda160246ad8c
Lunamandla <lunamandlaluna@gmail.com>
2023-07-18 18:40:16 -0700

    My 3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * main - function that check lowercase character
+ *
+ * Return: 1 for lowercase and 0 for anything else
+ *
+ */
+int_islower(int c)
+{
+	if (c => 97 && c <= 122)
+	{
+		return (1);
+	}
+		return (0);
+}
+
