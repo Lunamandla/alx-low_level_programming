a52ad5952bad00c2bcb3b9d87757d5c7472ac7ac
Lunamandla <lunamandlaluna@gmail.com>
2023-07-20 16:44:02 -0700

    My 0-isupper.c

--- 0x04-more_functions_nested_loops/_putchar.c
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- * main - writes the character c to 
+ * main - writes the character c to
  *
  * Return: (Succuess) 1
  */
 int _putchar(cahr c)
 {
-	return(write(1, &c, 1));
+	return (write(1, &c, 1));
 }
