a6b4cc9d8e30ee2d42626985427238c62f837e1b
Lunamandla <lunamandlaluna@gmail.com>
2023-07-19 17:23:51 -0700

    My 100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -5,9 +5,10 @@
  *
  * Return: Always 0 (Success)
  */
- void print_times_table(int n)
+void print_times_table(int n)
 {
 	int a, b, c;
+
 	if (n >= 0 && n <= 14)
 	{
 		for (a = 0; a <= n; a++)
@@ -29,11 +30,11 @@
 				_putchar(32);
 				_putchar(32);
 				_putchar(((c / 10) % 10) + '0');
-				_putchar(( c % 10) + '0');
+				_putchar((c % 10) + '0');
 			}
 			else 
 			{
-				if (b !=0)
+				if (b != 0)
 			{
 					_putchar(',');
 					_putchar(32);
