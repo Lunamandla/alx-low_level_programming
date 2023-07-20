0d22f4094390a64645faa010444f0e1bedfa6615
Lunamandla <lunamandlaluna@gmail.com>
2023-07-20 16:40:03 -0700

    My 0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _isupper - check if the letter is uppercase
+ *
+ * Return: 1 for uppercase and 0 for anything else
+ */
+int _isupper(int a)
+{
+	if (a >= 65 && a <= 90)
+	{
+	return (1);
+	}
+	return (0);
+}
---
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - writes the character c to 
+ *
+ * Return: (Succuess) 1
+ */
+int _putchar(cahr c)
+{
+	return(write(1, &c, 1));
+}
