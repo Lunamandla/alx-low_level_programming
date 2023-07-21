612244c18f7dedc3b7eea1449d643e370577e3c0
Lunamandla <lunamandlaluna@gmail.com>
2023-07-20 18:09:15 -0700

    My 3-print_numbers.c

--- 0x04-more_functions_nested_loops/_putchar.c
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -2,7 +2,7 @@
 /**
  * main - writes the character c to standard output
  * @c: The character to print
- *
+ * on error: -1 is returned and errno is send appropiatlely
  * Return: (Succuess) 1
  */
 int _putchar(char c)
