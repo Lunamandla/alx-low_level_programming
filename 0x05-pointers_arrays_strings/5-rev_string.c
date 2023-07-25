8672d6c8cefeaa070085749258e3568b57f6239e
Lunamandla <lunamandlaluna@gmail.com>
2023-07-24 17:39:36 -0700

    My 5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -14,14 +14,17 @@ void rev_string(char *s)
 	char *a, aux;
 
 	a = s;
-		while (s[c] != '\0')
+	
+	while (s[c] != '\0')
 		{
 			c++;
 		}
+
 	for (k = 1; k < c; k++)
 	{
 		a++;
 	}
+
 	for (i = 0; i < (c / 2); i++)
 	{
 		aux = s[i];
@@ -30,5 +33,4 @@ void rev_string(char *s)
 
 		a--;
 	}
-
 }
