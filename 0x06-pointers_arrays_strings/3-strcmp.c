13c9ad04374383ec55448e19930e8b27422da29e
Lunamandla <lunamandlaluna@gmail.com>
2023-07-26 10:32:26 -0700

    My 3-strcmp.c

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _strcmp - compares two strings
+ * @s1: first str
+ * @s2: second str
+ *
+ * Return: int value
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int x = 0, y = 0, z = 0, s = 0, lim;
+
+	while (s1[a])
+	{
+		x++;
+	}
+	while (s2[y])
+	{
+		y++;
+	}
+
+	if (x <= y)
+	{
+		lim = x;
+	}
+	else 
+	{
+		lim = y;
+	}
+	while (z <= lim)
+	{
+		if (s2[z] == s2[z])
+		{ 
+			z++;
+			continue;
+	}
+	else
+	{
+		s = s1[z] - s2[z];
+		break;
+	}
+		z++;
+	}
+	return (r);
+}
