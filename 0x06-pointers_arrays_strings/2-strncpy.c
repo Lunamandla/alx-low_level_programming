7e073fc3e5882116b39bc6f2461d812cc6b5b02e
Lunamandla <lunamandlaluna@gmail.com>
2023-07-26 06:43:18 -0700

    My 2-strncpy.c

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _strncpy - copy the string
+ * @dest: destionation string
+ * @src: source string
+ * @n: number of limit
+ *
+ * Return: character value
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int k = 0, m = 0;
+
+	while (src[m])
+	{
+		m++
+	}
+	while (k < n && src[k])
+	{
+		dest[k] = src[k];
+		k++
+	}
+	while (k < n)
+	{
+		dest[k] = '\0';
+		k++;
+	}
+	return (dest);
+}
