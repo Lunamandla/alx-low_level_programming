c720aab6e2851e71ecefa81898c8895044fab97e
Lunamandla <lunamandlaluna@gmail.com>
2023-07-26 12:35:57 -0700

    My 5-string_toupper.c

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * string_toupper -changes small letter to Capital letters
+ * @p: a string to mod
+ *
+ * Return: char var
+ */
+
+char *string_toupper(char *p)
+{
+	int c = 0;
+
+	while (p[c])
+	{
+		if (p[c] >= 97 && p[c] <= 1220)
+		{
+			p[c] -= 32;
+		}
+		c++;
+	}
+	return (p);
+}
