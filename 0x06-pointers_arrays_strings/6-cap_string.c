331f011edd32b6ca24daf129807664dca720948b
Lunamandla <lunamandlaluna@gmail.com>
2023-07-27 14:14:14 -0700

    My 6-cap_string

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * cap_string - Capitilizes all the letters
+ * @s: string to capitalized
+ *
+ * Return: char value
+ */
+
+char *cap_string(char *s)
+{
+	int b = 0, i;
+
+	int cspc = 13;
+	char spc[] = {32, '\t', '\n', 44, ';', 46, '!', '?', '"', '(', ')', '{', '}'};
+
+	while (s[b])
+	{
+		i = 0;
+	while (i < cspc)
+	{
+		if ((b == 0 || s[b - 1] == spc[i] && (s[b >= 97 && s[b <= 122))
+			s[a] -= 32;
+			i++;
+	}
+	b++;
+	}
+	return (s)
+}
