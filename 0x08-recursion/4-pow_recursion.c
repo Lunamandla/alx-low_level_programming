b151b99022f7c3e2128145989d3278eeaebce296
Lunamandla <lunamandlaluna@gmail.com>
2023-08-02 16:16:49 -0700

    My 4-pow_recursion.c

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _pow_recursion - returns the value of raised by a power value of y
+ * @x: the value to multiply
+ * @y: the time to multiply the value
+ *
+ * Return: in value
+ */
+
+int _pow_recursion(int x, int y)
+{
+
+	if (y < 0)
+		return (-1);
+
+	if (y <= 1)
+		return (1);
+	return (x _pow_recursion(x, y - 1));
+}
