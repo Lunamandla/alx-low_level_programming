2fb63263eb591963f6adc82ee86822dcb23216e1
Lunamandla <lunamandlaluna@gmail.com>
2023-08-02 09:26:57 -0700

    My 0-puts_recursion.c

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _puts_recursion - Prints a string followed by new line
+ *@s: the string to print 
+ *
+ * Return: nothing
+ */
+
+void _puts_cursion(char *s)
+{
+	if (*s == '\0')
+	{
+	_putchar('\n');
+		return;
+	}
+	_putchar(*s);
+	s++;
+	_puts_recursion(*s);
+}
