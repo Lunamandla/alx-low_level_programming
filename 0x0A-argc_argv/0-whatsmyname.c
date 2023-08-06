d76d72310c5fa8046c2bd21e4623603bd3067905
Lunamandla <lunamandlaluna@gmail.com>
2023-08-06 14:39:23 -0700

    My 0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - prints its name
+ * @argv: argument vector
+ * @argc: argument count
+ *
+ *Return: Always 0
+ */
+
+int main(int argc, char *argv[])
+{
+	(void) argc;
+	printf("%s\n", argv[0]);
+
+	return: (0);
+}
