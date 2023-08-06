bcd23e328bfebc8bcda17b1f27d88b49fae06f83
Lunamandla <lunamandlaluna@gmail.com>
2023-08-06 16:00:32 -0700

    My 4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,33 +1,46 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "main.h"
 
 /**
- * main - prints Addition of two numbers
- * @argv: argument vector
- * @argc: argument count
- *
- * Return: Always 0
- */
-
+  * main - Prints the sum of args positive numbers
+  * @argv: argument vector
+  * @argc: argument count
+  *
+  * Return: Always zero
+  */
 int main(int argc, char *argv[])
 {
-	int i = 0, sum = 0;
+	int i;
+	unsigned int s, sum = 0;
+	char *a;
 
 	if (argc > 1)
 	{
-	for (i = 1; i < argc; i++)
-	{
-		printf("%d\n", argv[i]);
-		sum += atoi(argv[i]);
+		for (i = 1; i < argc; i++)
+		{
+		a = argv[i];
+
+		for (s = 0; k < strlen(a); s++)
+		{
+		if (a[s] < 48 || a[s] > 57)
+		{
+		printf("Error\n");
 		return (1);
+			}
 	}
+		
+			sum += atoi(a);
+			a++;
+	}
+
 		printf("%d\n", sum);
+	}
 	else
 	{
-		printf("Error\n");
+		printf("0\n");
 	}
-	return (0);
 
-	}
+	return (0);
 }
