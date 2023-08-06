4f7503e500fb455bb0cc009ff173dae670031d3b
Lunamandla <lunamandlaluna@gmail.com>
2023-08-06 15:47:52 -0700

    My 4-add.c

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * main - prints Addition of two numbers
+ * @argv: argument vector
+ * @argc: argument count
+ *
+ * Return: Always 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int i = 0, sum = 0;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%d\n", argv[i]);
+		sum += atoi(argv[i]);
+		return (1);
+	}
+	else
+	{
+		printf("Error\n");
+	}
+	return (0);
+}
