99eacc97db2dd2868e29ac7dd9792e3e39ae6c70
Lunamandla <lunamandlaluna@gmail.com>
2023-08-06 15:10:26 -0700

    My 2-args.c

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - prints all arguments that recieved 
+ * @argv: argument vector
+ * @argc: argument count
+ *
+ * Retuen: Always 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0, i < argc, i++)
+	{
+		printf("%d\n", argv[i]);
+	}
+	return (0)
+}
