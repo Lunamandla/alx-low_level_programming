4f4784691f2e8e87aa0eb1a61efe2fcd3f4a70b4
Lunamandla <lunamandlaluna@gmail.com>
2023-08-07 13:27:46 -0700

    My 0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -18,6 +18,11 @@ char *create_array(unsigned int size, char c)
 	if (size == 0)
 	return (NULL);
 
+	a = malloc(size * sizeof(char));
+
+	if (a == NULL)
+		retur (NULL);
+
 	for (i = 0; i < size;i++)
 	{
 		a[i] = c;
