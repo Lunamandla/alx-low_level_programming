112bb6f37b0ff2f2127120d4c71541c6cf47e540
Lunamandla <lunamandlaluna@gmail.com>
2023-08-10 16:53:09 -0700

    My 1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -13,45 +13,39 @@
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int x, y, z;
+	unsigned int x = 0, y = 0, len1 = 0, len2 = 0;
 	char *s;
 	
-	if (s1 == NULL)
-	{
-		x = 0;
-	}
+	while (s1 && s1[len1])
+
+		len1++;
+
+	while (s2 && s2[len2])
+
+		len2++;
+
+	if (n < len2)
+
+		s = malloc(sizeof(char) *(len1 + n + 1));
+
 	else
-	{
-		for (x = 0; s1[x]; ++x)
-			++x;
-	}
-	if (s2 == NULL)
-	{
-		y = 0;
-	}
-	else
-	{
-		for (y = 0; s2[y]; ++y)
-			++y;
-	}
-	if (y > n)
 
-	y = n;
+		s = malloc(sizeof(char) * (len1 + len2 + 1));
 
-	s = malloc(sizeof(char) * (x + y + 1));
-
-	if (s == NULL)
+	if (!s)
 
 		return (NULL);
 
-	for (z = 0; z < x; z++)
+	while (x < len1)
+	{
+	s[x] = s1[x];
+	}
+	while (n < len2 && x < (len1 + n))
+		s[x++] = s2[y++];
 
-		s[z] = s1[z];
+	while (n >= len2 && x < (len1 + len2))
+		s[x++] = s2[y++];
+			s[x] = '\0';
 
-	for (z = 0; z < y; z++)
-
-			s[z + x] = s2[z];
-			s[x + y] = '\0';
-
-		return (s);
+			return (s);
 }
