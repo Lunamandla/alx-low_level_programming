bfc40ba8ee6dd190f1776e5fe6245857987a1af6
Lunamandla <lunamandlaluna@gmail.com>
2023-08-10 17:51:31 -0700

    My 2-calloc.c

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -13,11 +13,11 @@
 
 char *_memset(char *s, char b, unsigned int n)
 {
-	unsigned int a;
-	
-	for (a < 0; a < n; a++)
+	unsigned int i;
+
+	for (i < 0; i < n; i++)
 	{
-		s[a] = b;
+		s[i] = b;
 	}
 
 	return (s);
@@ -39,6 +39,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (nmemb == 0 || size == 0)
 
 		return (NULL);
+
 	ptr = malloc(size * nmemb);
 
 	if (ptr == NULL)
