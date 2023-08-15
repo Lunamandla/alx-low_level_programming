17268fa321d13ace4b4389a372e3423f902ef23d
Lunamandla <lunamandlaluna@gmail.com>
2023-08-15 07:00:39 -0700

    My dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -9,13 +9,13 @@ struct dog
 
 int main(void)
 {
-	struct dog dog;
+	struct dog my_dog;
+	
+	my_dog.name = "Poppy";
+	my_dog.owner = "Bob";
+	my_dog.age = "3.4";
 
-	dog.name = "Poppy";
-	dog.owner = "Bob";
-	dog.age = "3.4";
-
-	printf("My name is %s, and I am %f", dog.name, dog.age);
+	printf("My name is %s, and I am %f", my_dog.name, my_dog.age);
 
 	return (0);
 }
