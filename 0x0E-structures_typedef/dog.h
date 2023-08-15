2829e0854cc1a87a63fbe184619eb85c5521e980
Lunamandla <lunamandlaluna@gmail.com>
2023-08-15 12:18:51 -0700

    My dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -16,12 +16,14 @@ struct dog
 };
 
 /**
- * dog_t - Typedef of struct dog
+ * dog_p - Typedef of struct dog
  */
 
-void init(struct dog *d, char *name, float age, char *owner);
+typedef struct dog dog_p
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
 	void print_dog(struct dog *d);
-	dog_t *new_dog(char *name, float age, char *owner);
-	void free_dog(dog_t *d);
+	dog_p *new_dog(char *name, float age, char *owner);
+	void free_dog(dog_p *d);
 #endif
 	
