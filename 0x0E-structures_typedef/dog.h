02b91318c25b8e946d5dc0bd1b531aebadc2879b
Lunamandla <lunamandlaluna@gmail.com>
2023-08-15 08:08:32 -0700

    My dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -8,8 +8,8 @@ void init_dog(struct dog *my_dog, char *name, float age, char *owner)
 	if (my_dog == NULL)
 		my_dog = malloc(sizeof(struct dog));
 
-	my_dog.name = name;
-	my_dog.age = age;
-	my_dog.owner = owner;
+	my_dog->name = name;
+	my_dog->age = age;
+	my_dog->owner = owner;
 
 }
