90d2cc8e5e92ab09edf1590412c5f21b57a6a795
Lunamandla <lunamandlaluna@gmail.com>
2023-08-15 11:05:10 -0700

    My dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,24 +1,12 @@
 #include "dog.h"
 #include <stdlib.h>
 
-struct dog;
+void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	char *name;
-	float age;
-	char *owner;
-
-};
-
-struct dog *ptr(char *name, float age, char *owner)
-{
-	struct dog *ptr;
-
-	ptr = malloc(sizeof(struct dog));
-	if (ptr == NULL)
-	return (NULL);
-
-	ptr->name = name;
-	ptr->age = age;
-	ptr->owner = owner;	
-	return (ptr);
+	if (d == NULL)
+		d = malloc(sizeof(struct dog));
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
 }
+
