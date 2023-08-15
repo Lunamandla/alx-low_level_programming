a2be76237f0103650950f4a3d505342e88b0c8f6
Lunamandla <lunamandlaluna@gmail.com>
2023-08-15 13:40:33 -0700

    My 1-init_dog.c

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -3,18 +3,18 @@
 
 /**
  * init_dog - initialize variable  of struct dog
- * @p: the dog to be initialized
+ * @d: the dog to be initialized
  * @name: name of the dog
  * @age: age of the dog
  * @ower: dog's owner's name
  */
 
-void init_dog(struct dog *p, char *name, float age, char *owner)
+void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	if (p != NULL)
+	if (d != NULL)
 	{
-		p->name = name;
-		p->age = age;
-		p->owner = owner;
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
 	}
 }
