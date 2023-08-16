ecf312dd9f341ed0fba93730076e0eaf143e104f
Lunamandla <lunamandlaluna@gmail.com>
2023-08-16 13:22:06 -0700

    My 0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -4,12 +4,13 @@
 
 /** print_name - function that prints name
  * @name: name of the user
+ * @f: pointer to func
  * return: nothing
  */
 
 void print_name(char *name, void (*f)(char *));
 {
 	if  (name  == NULL || f == NULL)
-		return (0);
+		return;
 	f(name);
 }
