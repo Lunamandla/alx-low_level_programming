a9b171cf58546bee92b6c475f90702a99a119129
Lunamandla <lunamandlaluna@gmail.com>
2023-08-16 15:38:48 -0700

    My 2-int_index.c

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,24 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - searches for integers
+ * @size: size of an array
+ * @cmp: pointer that points a function of one of the 3 main
+ * Return: 0
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int  a;
+
+	if (array == NULL || size <= 0 || cmp = NULL)
+		return (-1);
+
+	for (a = 0; a < size; a++)
+	{
+		if (cmp(array[a]))
+			return (a);
+	}
+	return (-1);
+		
+}
