d77a3ca882c6fb720dd4d69023f4b1d89b17d5f9
Lunamandla <lunamandlaluna@gmail.com>
2023-08-18 03:52:21 -0700

    My 2-print_strings.c

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -25,8 +25,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		if (str)
 			printf("%s", str);
 		else
-			print("(nil)");
-
+			print('(nil)');
 
 		if (a < n -1)
 			if (separator)
