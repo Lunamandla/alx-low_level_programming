7ac174bcb0579fedff4b9bd785cfef0ca51620b4
Lunamandla <lunamandlaluna@gmail.com>
2023-08-31 15:40:58 -0700

    My 0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -17,8 +17,9 @@ unsigned int binary_to_uint(const char *b)
 
 	for (i = 0; b[i]; i++)
 	{
-		if (b[i]i< '0' || b[i] > '1')
+		if (b[i]i < '0' || b[i] > '1')
 			return (0);
 		dec_val = 2 * dec_val + (b[i] - '0');
 	}
+	return dec_val;
 }
